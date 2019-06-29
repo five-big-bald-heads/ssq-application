8106f27fd9bd5bbb5bf1a40955e62dac112b2b47
xiaoroulian <494337840@qq.com>
2019-06-29 10:03:52 +0800

    个人中心
    个人中心页面，修改密码，查看个人信息，检查更新

--- src/pages/PersonalCenter.vue
+++ src/pages/PersonalCenter.vue
@@ -5,15 +5,23 @@
     </div>
     <div class="content">
       <div class="img_area">
+        <x-button class="buttontype" action-type="button" @click.native="actionsheet">
         <span class="span1">
-          <router-link to="/ModifyImg">
-            <img class="img" src="../assets/img.jpeg">
-          </router-link>
+            <img class="img" src="../assets/icon.jpg">
         </span>
-        <span>
-
+          <span class="span2">
+          <div class="name">{{name}}</div>
+          <div class="number">{{userName}}</div>
         </span>
+        </x-button>
       </div>
+      <actionsheet
+        :menus= "data"
+        v-model="sheetVisible"
+        @on-click-menu-menu1="getCamera"
+        @on-click-menu-menu2="getImage"
+        show-cancel>
+      </actionsheet>
       <div class="list">
         <group label-width="5.5em" label-margin-right="2em" label-align="justify">
           <cell-box is-link  link="/PersonalInfo">
@@ -22,7 +30,7 @@
           <cell-box is-link  link="/ModifyPassword">
             <img src="../assets/pwd.png" style="margin-right: 10px">修改密码
           </cell-box>
-          <cell-box is-link  link="/">
+          <cell-box is-link  link="/CheckUpdate">
             <img src="../assets/update.png" style="margin-right: 10px">检查更新
           </cell-box>
           <cell-box is-link  link="/AboutUs">
@@ -38,7 +46,8 @@
 </template>
 
 <script>
-import { Group, Cell, XHeader, XInput, XButton, CellBox } from 'vux'
+import { Group, Cell, XHeader, XInput, XButton, CellBox, Actionsheet } from 'vux'
+import axios from 'axios'
 export default {
   name: 'PersonalCenter',
   components: {
@@ -47,7 +56,44 @@ export default {
     XHeader,
     XInput,
     XButton,
+    Actionsheet,
     CellBox
+  },
+  data () {
+    return {
+      // action sheet 选项内容
+      userName: '',
+      name: '',
+      sheetVisible: false,
+      data: {
+        menu1: '拍照',
+        menu2: '从相册中选择'
+      }
+    }
+  },
+  mounted () {
+    this.showData()
+  },
+  methods: {
+    actionsheet () {
+      this.sheetVisible = true
+    },
+    getCamera () {
+      console.log('拍照')
+    },
+    getImage () {
+      console.log('相册')
+    },
+    showData () {
+      this.userName = sessionStorage.getItem('username')
+      axios.get('http://localhost:8080/Self', {
+        stno: this.userName
+      }).then((response) => {
+        this.name = response.data.data.name
+      }, (response) => {
+        // 响应错误回调
+      })
+    }
   }
 }
 </script>
@@ -61,19 +107,41 @@ export default {
   }
   .img_area{
     background-color: #ffffff;
-    height: 120px;
+    height: 110px;
+  }
+  .buttontype{
+    background-color: #ffffff;
+    outline: none;
+    width: 100%;
+    border:none;
+  }
+  .buttontype::after{
+    border: none
   }
   .span1 {
     display:inline-block;
-    position: absolute;
+    float: left;
     height: 100px;
+  }
+  .span2 {
+    display:inline-block;
+    margin-right: 170px;
+    margin-top: 7px;
+    float: right;
+    height: 100px;
+  }
+  .name{
+    float: left;
+  }
+  .number{
+    margin-top: 50px;
   }
   .img{
     position: relative;
     width: 80px;
     height: 80px;
-    margin-left: 20px;
-    margin-top: 20px;
+    margin-left: 10px;
+    margin-top: 15px;
   }
   .cell {
     padding-top: 15px;
@@ -91,14 +159,14 @@ export default {
   }
 
   /*.img {*/
-    /*display: block;*/
-    /*margin-right: 15px;*/
+  /*display: block;*/
+  /*margin-right: 15px;*/
   /*}*/
-    /*.aa{*/
-      /*position: absolute;*/
-      /*margin: 10px;*/
-      /*width: 100%;*/
-      /*height: 130px;*/
-      /*background-color: white;*/
-    /*}*/
+  /*.aa{*/
+  /*position: absolute;*/
+  /*margin: 10px;*/
+  /*width: 100%;*/
+  /*height: 130px;*/
+  /*background-color: white;*/
+  /*}*/
 </style>
